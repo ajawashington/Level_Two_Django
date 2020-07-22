375ddabe6e7ac79d223f0c82af4419986d6a0ec5
Aja Washington <aja.washington13@gmail.com>
2020-07-22 09:54:55 -0500

    inital commit

---
+++ first_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from first_app.models import AccessRecord, Topic, Webpage
+
+# Create your views here.
+
+def index(request):
+    #get all webpages and ordering by date on access record
+    webpages_list = AccessRecord.objects.order_by('date')
+
+    #define template tag
+    date_dict = {'access_records':webpages_list}
+    return render(request,'first_app/index.html',context=date_dict)
